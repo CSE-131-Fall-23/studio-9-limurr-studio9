8f132c21539e53d4273f6512295e50702cacb905
benkotch <benkotch@macbook-air-261.dhcp.wustl.edu>
2023-11-30 10:38:44 -0600

    Thanks!

--- src/studio9/WordCount.java
+++ src/studio9/WordCount.java
@@ -8,32 +8,16 @@ import java.util.Map;
 import support.cse131.NotYetImplementedException;
 
 public class WordCount {
-	/**
-	 * Constructs and returns a map of the distinct words in the specified list with
-	 * each word associated with its accumulated count (that is: the number of
-	 * occurrences of the word in the list).
-	 * 
-	 * For example, if passed a List<String> containing:
-	 * 
-	 * [to, be, or, not, to, be]
-	 * 
-	 * the resulting Map<String, Integer> would contain
-	 * 
-	 * key="to", value=2;
-	 * key="be", value=2;
-	 * key="or", value=1;
-	 * key="not", value=1;
-	 * 
-	 * @param words
-	 * @return a map which contains all of the distinct words as keys, each
-	 *         associated with the number of occurrences of the word
-	 */
-	public static Map<String, Integer> countWords(List<String> words) {
+	 public static Map<String, Integer> countWords(List<String> words) {
+	        Map<String, Integer> wordCountMap = new HashMap<>();
 
-		// FIXME
-		throw new NotYetImplementedException();
+	        for (String word : words) {
+	            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+	        }
 
-	}
+	        return wordCountMap;
+	    }
+
 	
 	public static void main(String[] args) {
 		
@@ -46,7 +30,8 @@ public class WordCount {
 		wordList.add("be");
 		Map<String, Integer> words = countWords(wordList);
 		
-		//TODO: Write code that will iterate over the words map
-		//to verify its contents
-	}
+		for (Map.Entry<String, Integer> entry : words.entrySet()) {
+            System.out.println("Word: " + entry.getKey() + ", Count: " + entry.getValue());
+        }
+    }	
 }
